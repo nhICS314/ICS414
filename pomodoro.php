c2a042a04843d7b284f0ffde99069b65e2a08178
April <ara814@hawaii.edu>
2016-11-17 12:19:18 -1000

    update on timer page

--- pomodoro.php
+++ pomodoro.php
@@ -6,19 +6,30 @@
     <title>Pomodoro</title>
 </head>
 <body>
-<div id="timer">
-    <span id="minutes">25</span>
-    <span id="middle">:</span>
-    <span id="seconds">00</span>
-    <span id="message"></span>
+
+<div id="task">
+    <u>Task Details</u>
+    <br/>
 </div>
-<div id="buttons">
-    <button id="start">start</button>
-    <button id="stop">stop</button><br/>
-    <button id="reset">reset</button><br/>
-    <button id="settings">settings</button><br/><br/>
-    <button id="rev">submit for review</button><br/>
+<div id="container">
+    <div id="timer">
+        <span id="minutes">25</span>
+        <span id="middle">:</span>
+        <span id="seconds">00</span>
+        <span id="message"></span>
+    </div>
+
+     <div id="buttons">
+        <button id="start">start</button>
+        <button id="stop">stop</button>
+        <button id="reset">reset</button>
+        <br/><br/>
+        <button id="settings">settings</button>
+        <br/><br/>
+        <button id="rev">submit for review</button>
+    </div>
 </div>
+
 <script type="text/javascript" src="pom.js"></script>
 </body>
 </html>
